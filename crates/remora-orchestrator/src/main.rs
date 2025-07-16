0ca9b49aaecc21a757e2dfe859b0ce717241ff46
Scofield626 <scofield_zliu@outlook.com>
2025-07-16 17:10:32 +0100

    cherry-pick: orch build

--- crates/remora-orchestrator/src/main.rs
+++ crates/remora-orchestrator/src/main.rs
@@ -90,15 +90,13 @@ pub enum Operation {
         #[clap(long, action, default_value_t = false, global = true)]
         skip_testbed_configuration: bool,
     },
-    /// Build the binaries on all the instances of the testbed.
-    Build {
-        /// Whether to skip testbed updates before building the binaries. This is a dangerous
-        /// operation as it may lead to building binaries on outdated nodes. It is however
-        /// useful when debugging in some specific scenarios.
-        #[clap(long, action, default_value_t = false, global = true)]
-        skip_testbed_update: bool,
-    },
     GenerateLog,
+    /// Build the binaries on all the instances of the testbed.Add commentMore actions
+    Build {
+        /// The path to the settings file.
+        #[clap(long, action, default_value_t = false, global = true)]
+        install_dependencies: bool,
+    },
     /// Print a summary of the specified measurements collection.
     Summarize {
         /// The path to the settings file.
@@ -261,8 +259,9 @@ async fn run<C: ServerProviderClient>(
             .await
             .wrap_err("Failed to run benchmarks")?;
         }
-
-        Operation::Build { skip_testbed_update } => {
+        Operation::Build {
+            install_dependencies,
+        } => {
             // Only build the binaries on all the instances.
             let username = testbed.username();
             let private_key_file = settings.ssh_private_key_file.clone();
@@ -279,17 +278,25 @@ async fn run<C: ServerProviderClient>(
 
             let protocol_commands = Protocol::new(&settings);
 
-            Orchestrator::new(
+            let mut orchestrator = Orchestrator::new(
                 settings,
                 instances,
                 setup_commands,
                 protocol_commands,
                 ssh_manager,
-            )
-            .skip_testbed_update(skip_testbed_update)
-            .update()
-            .await
-            .wrap_err("Failed to update testbed")?;
+            );
+
+            if install_dependencies {
+                orchestrator
+                    .install()
+                    .await
+                    .wrap_err("Failed to install dependencies")?;
+            }
+
+            orchestrator
+                .update()
+                .await
+                .wrap_err("Failed to update testbed")?;
         }
 
         Operation::GenerateLog => {
